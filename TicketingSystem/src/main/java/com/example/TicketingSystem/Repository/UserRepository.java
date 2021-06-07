de205dd6a7291c9a94f9d637b32bb30e19ad9909
noufaljufair <60816253+noufaljufair@users.noreply.github.com>
2021-06-07 14:59:40 +0300

    userService

--- TicketingSystem/src/main/java/com/example/TicketingSystem/Repository/UserRepository.java
+++ TicketingSystem/src/main/java/com/example/TicketingSystem/Repository/UserRepository.java
@@ -2,6 +2,31 @@ package com.example.TicketingSystem.Repository;
 
 import com.example.TicketingSystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User ,Long> {
+
+
+    //User findByid(Long id);
+    User findBytype(int type);
+
+ /*
+    @Modifying
+    @Query(value = "update users set password =:password  where id =:userId", nativeQuery = true)
+    @Transactional
+    void changePassword(@Param("userId") Long userId, @Param("password") String password);
+
+    @Modifying
+    @Query(value = "update users set password =:password, active =:active  where id =:userId", nativeQuery = true)
+    @Transactional
+    void editUser(@Param("userId") Long userId, @Param("password") String password,@Param("active") boolean active);
+*/
+
+
+
 }
--- TicketingSystem/src/main/java/com/example/TicketingSystem/service/UserService.java
+++ TicketingSystem/src/main/java/com/example/TicketingSystem/service/UserService.java
@@ -1,4 +1,65 @@
 package com.example.TicketingSystem.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.example.TicketingSystem.Repository.UserRepository;
+import com.example.TicketingSystem.model.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
 public class UserService {
+
+
+   @Autowired
+   private UserRepository userRepository;
+
+
+
+   public List<User> getAllUsers(){
+      List<User> users = new ArrayList<>();
+      userRepository.findAll().forEach(users::add);
+      return users;
+   }
+
+
+   public User getUserById(long id){
+      List<User> users = new ArrayList<>();
+      userRepository.findAll().forEach(users::add);
+      return users.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+ }
+
+   public List<User> getUserByType(int type){
+
+      List<User> users = new ArrayList<>();
+      userRepository.findAll().stream().filter(u -> u.getType() == type).forEach(users::add);
+      return users;
+}
+
+
+   public void addUser(User user){
+      userRepository.save(user);
+   }
+
+   public void updateUser(User user){
+      userRepository.save(user);
+   }
+
+
+   public void deleteUser(long id){
+      userRepository.deleteById(id);
+   }
+
+
+
 }
