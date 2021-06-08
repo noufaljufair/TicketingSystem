b337329f4d039240fdf470a5aeb37c602d5b6192
fawziahAlaqil <85229759+fawziahAlaqil@users.noreply.github.com>
2021-06-08 14:40:56 +0300

    Update UsersController.java

--- TicketingSystem/src/main/java/com/example/TicketingSystem/controller/UsersController.java
+++ TicketingSystem/src/main/java/com/example/TicketingSystem/controller/UsersController.java
@@ -1,13 +1,14 @@
 package com.example.TicketingSystem.controller;
 
 
-import com.example.TicketingSystem.model.Ticket;
 import com.example.TicketingSystem.model.User;
 import com.example.TicketingSystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+
+//import org.springframework.security.authentication.AuthenticationManager;
 
 
 @RestController
@@ -24,13 +25,13 @@ public class UsersController {
     }
 
 //updateUser
-    @RequestMapping(method=RequestMethod.POST, value = "/users/{id}")
+    @RequestMapping(method=RequestMethod.PUT, value = "/users/{id}")
     public void addTopic(@RequestBody User user , @PathVariable long id ){
         userService.updateUser(user);
     }
 
 //delete user
-    @RequestMapping(method=RequestMethod.POST, value = "/users/{id}")
+    @RequestMapping(method=RequestMethod.DELETE, value = "/users/{id}")
     public void deleteTopic(@PathVariable long id ){ userService.deleteUser(id); }
 
 
