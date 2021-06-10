a5fc44a5a94175d2ba81f4e395be06aa6fb5116f
Lama AlMayouf <lamayf@hotmail.com>
2021-06-10 11:02:19 +0300

    Security dependency

--- TicketingSystem/src/main/java/com/example/TicketingSystem/configuration/AuditorBean.java
+++ TicketingSystem/src/main/java/com/example/TicketingSystem/configuration/AuditorBean.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@Configuration
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
-public class AuditorBean {
-    @Bean
-    public AuditorAware<String> auditorAware(){
-        return new AuditorAwareImpl();
-    }
-}
+//@Configuration
+//@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+//public class AuditorBean {
+//    @Bean
+//    public AuditorAware<String> auditorAware(){
+//        return new AuditorAwareImpl();
+//    }
+//}
