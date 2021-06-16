9dd232d65231e84b1e40a4a787f8d61cbabeb4db
Lama AlMayouf <lamayf@hotmail.com>
2021-06-16 09:06:33 +0300

    added Translator and updates on LocalResolverConfig

--- TicketingSystem/src/main/java/com/example/TicketingSystem/configuration/LocaleResolver.java
+++
@@ -1,9 +0,0 @@
-package com.example.TicketingSystem.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-
-@Configuration
-public class LocaleResolver extends AcceptHeaderLocaleResolver implements WebMvcConfigurer {
-}
---
+++ TicketingSystem/src/main/java/com/example/TicketingSystem/configuration/LocaleResolverConfiguration.java
@@ -0,0 +1,47 @@
+package com.example.TicketingSystem.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class LocaleResolverConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.forLanguageTag("ar"));
+        cookieLocaleResolver.setCookieName("language");
+
+        return cookieLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("i18n/messages");
+        resourceBundleMessageSource.setDefaultEncoding("UTF-8");
+        resourceBundleMessageSource.setUseCodeAsDefaultMessage(true);
+        return resourceBundleMessageSource;
+    }
+}
