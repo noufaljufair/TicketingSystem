e7d6b303afca22e128136704af0742b60fbc5590
fawziahAlaqil <85229759+fawziahAlaqil@users.noreply.github.com>
2021-06-16 11:48:42 +0300

    Security configuration

--- TicketingSystem/src/main/java/com/example/TicketingSystem/security/SpringSecurityConfiguration.java
+++ TicketingSystem/src/main/java/com/example/TicketingSystem/security/SpringSecurityConfiguration.java
@@ -2,14 +2,39 @@ package com.example.TicketingSystem.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfiguration extends WebSecurityConfiguration {
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+   // @Override
+   // protected void configure(HttpSecurity http) throws Exception {
+       // http
+           //     .authorizeRequests()
+            //    .antMatchers("/", "/users").permitAll()
+             //   .anyRequest().authenticated()
+              //  .and()
+              //  .formLogin()
+              //  .loginPage("/login")
+              //  .permitAll()
+              //  .and()
+              //  .logout()
+               // .permitAll(); }
 
+   // @Bean
+    //@Override
+  // public UserDetailsService userDetailsService() {
+       // UserDetails user = withDefaultPasswordEncoder()
+                     //   .username("email")
+                      //  .password("password")
+                      //  .roles("USER")
+                       // .build();
 
+       // return new InMemoryUserDetailsManager(user);  }
 }
+
+
+
+
