cecb2f7f0bedcf2f939649ac4e4c392e9db1b4b7
fawziahAlaqil <85229759+fawziahAlaqil@users.noreply.github.com>
2021-06-16 10:50:26 +0300

    Update pom.xml

---
+++ TicketingSystem/src/main/java/com/example/TicketingSystem/security/SpringSecurityConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.TicketingSystem.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration extends WebSecurityConfiguration {
+
+
+
+}
