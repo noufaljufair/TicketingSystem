d408321cfb8000e380d3f690aeaf83601bdac24c
Lama AlMayouf <lamayf@hotmail.com>
2021-06-23 11:16:30 +0300

    handled Data Integrity Violation Exception (email in use)

--- TicketingSystem/src/main/java/com/TicketingSystem/exception/ApiRequestExceptionHandler.java
+++ TicketingSystem/src/main/java/com/TicketingSystem/exception/ApiRequestExceptionHandler.java
@@ -1,11 +1,21 @@
 package com.TicketingSystem.exception;
 
+import com.TicketingSystem.configuration.Translator;
 import com.TicketingSystem.dto.ErrorResponse;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,6 +24,7 @@ public class ApiRequestExceptionHandler{
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        System.out.println("!!!");
         List<ErrorModel> errorMessage = exception.getBindingResult()
                 .getFieldErrors()
                 .stream()
@@ -25,5 +36,20 @@ public class ApiRequestExceptionHandler{
         ErrorResponse errorResponse = ErrorResponse.builder().errorMessage(errorMessage).build();
 
         return new ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST);
-}
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException exception){
+        String message = exception.getRootCause().getMessage();
+        List<ErrorModel> errorMessage;
+
+        if(message != null && message.contains("duplicate") && message.contains("email")){
+            errorMessage = Arrays.asList(new ErrorModel("email","", Translator.toLocale("error.user.email.inUse")));
+        }else{
+            errorMessage = Arrays.asList(new ErrorModel("","",message));
+        }
+
+        ErrorResponse errorResponse = ErrorResponse.builder().errorMessage(errorMessage).build();
+        return new ResponseEntity(errorResponse, HttpStatus.CONFLICT);
+    }
 }
