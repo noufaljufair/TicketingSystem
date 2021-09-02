82b95c99bd4e0ba0e19f09af3ff03c1db8e387d2
Lama AlMayouf <lamayf@hotmail.com>
2021-09-02 10:27:11 +0300

    update CorsConfiguration

--- TicketingSystem/src/main/java/com/TicketingSystem/security/SecurityConfiguration.java
+++ TicketingSystem/src/main/java/com/TicketingSystem/security/SecurityConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -19,9 +18,8 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -69,31 +67,39 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and().logout().logoutUrl("logout")
                 .and().exceptionHandling().accessDeniedHandler(accessDeniedHandler)
                 .authenticationEntryPoint(authenticationEntryPoint)
+                .and().cors()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
-        http.cors(c -> {
-            CorsConfigurationSource cs = r -> {
-                CorsConfiguration cc = new CorsConfiguration();
-                cc.setAllowedOrigins(List.of("http://localhost:4200/"));
-                cc.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-                return cc;
-            };
-            CorsConfigurer ss = new CorsConfigurer();
-            c.configurationSource(cs);
-        });
+//        http.cors(c -> {
+//            CorsConfigurationSource cs = r -> {
+//                CorsConfiguration cc = new CorsConfiguration();
+//                cc.setAllowedOrigins(List.of("http://localhost:4200"));
+//                cc.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+//
+//                return cc;
+//            };
+//            CorsConfigurer ss = new CorsConfigurer();
+//            c.configurationSource(cs);
+//        });
     }
 
 //    @Bean
 //    CorsConfigurationSource corsConfigurationSource() {
 //        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
-//        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-//       // UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        //source.registerCorsConfiguration("/**", configuration);
-//        CorsConfigurer ss = new CorsConfigurer();
-//        return ss.configurationSource(configuration);
+//        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+//        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
 //    }
+@Bean
+CorsConfigurationSource corsConfigurationSource() {
+    UrlBasedCorsConfigurationSource source = new
+            UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    return source;
+}
 
 }
 
